d9cd455ee81d5ee844d756871af7a11a333585b7
litokele <litokele2018@gmail.com>
2020-03-28 17:14:02 +0800

    90_关键字高亮

--- test.js
+++ test.js
@@ -1,42 +1,14 @@
-var hasGroupsSizeX = function(deck) {
-  let set = new Set()
-  let uni = []
-
-  deck.forEach(item => {
-    if (!set.has(item)) {
-      set.add(item)
-      uni.push(item)
-    }
-  })
-  let arr = []
-  for (let i = 0, length = uni.length; i < length; i ++) {
-      let count = 0
-      for (let j = 0; j < deck.length; j ++) {
-          if (uni[i] === deck[j]) {
-              count ++
-          }
-      }
-      arr.push(count)
-  }
-  arr.sort((a, b) => {
-      return a - b
-  })
+var isPalindrome = function(x) {
+  if (x === 0) return true
   debugger
-  if (arr[0] === 1) {
-      return false
-  }
-  for (let j = 2; j <= arr[0]; j ++) {
-    let count = 0
-    for (let i in arr) {
-      if (arr[i] % j === 0) {
-        count ++
-      }
-      if (count === arr.length) {
-        return true
-      }
-    }
-  }
-  return false
-};
 
-hasGroupsSizeX([1,2,3,4,4,3,2,1])
+  if (x < 0 || x % 10 !== 0) return false
+  let reverseNum = 0
+  while (x > reverseNum) {
+
+      reverseNum = reverseNum * 10 + x % 10
+      x = Math.floor(x / 10)
+  }
+  return reverseNum === x || Math.floor(reverseNum / 10) === x  
+};
+isPalindrome(121)
