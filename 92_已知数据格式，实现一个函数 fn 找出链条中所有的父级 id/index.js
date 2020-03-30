18e5a1a52cc29da1477721b8c9cfc5e62e94afb5
litokele <litokele2018@gmail.com>
2020-03-30 16:55:58 +0800

    95_实现深拷贝

---
+++ 92_已知数据格式，实现一个函数 fn 找出链条中所有的父级 id/index.js
@@ -0,0 +1,34 @@
+let root = [
+  {
+    id: '1',
+    name: '广东省',
+    children: [
+      {
+        id: '11',
+        name: '深圳市',
+        children: [
+          {
+            id: '111',
+            name: '南山市'
+          },
+          {
+            id: '112',
+            name: '福田区' 
+          }
+        ]
+      }
+    ]
+  }
+]
+const value = '111'
+const fn = (value) => {
+  let result = []
+  value = value * 1
+  while (value >= 1) {
+    result.push(value)
+    value = Math.floor(value / 10)
+  }
+  result.sort()
+  return result
+}
+console.log(fn(value)) // 输出 [1， 11， 112]
---
+++ 94_vue 在 v-for 时给每项元素绑定事件需要用事件代理吗？为什么？/index.js
@@ -0,0 +1,9 @@
+/* 
+https://github.com/Advanced-Frontend/Daily-Interview-Question/issues/145
+  Vue 本身没有 进行事件代理
+
+  事件代理作用主要是 2 个
+
+  将事件处理程序代理到父节点，减少内存占用率
+  动态生成子节点时能自动绑定事件处理程序到父节点
+*/
