666158d6ca37e3cac376583b024314eb36361e55
litokele <litokele2018@gmail.com>
2020-04-13 22:14:27 +0800

    2020.04.13_多标签通信

---
+++ 109_Day_08_2020_04.13清除浮动和离线储存/[js] 写一个加密字符串的方法.js
@@ -0,0 +1,29 @@
+/* [js] 写一个加密字符串的方法 */
+function encodeStr(str, key) {
+  return str
+    .split("")
+    .map(item => {
+      return item.charCodeAt() * key;
+    })
+    .join("#");
+}
+
+function decodeStr(str, key) {
+  return str
+    .split("#")
+    .map(item => {
+      return String.fromCharCode(+item / key);
+    })
+    .join("");
+}
+let s = 'litokele'
+let m = encodeStr(s, 2333119)
+debugger
+// 利用 base64, 浏览器环境自带 btoa / atob 方法
+function encode (str) {
+	return btoa(encodeURIComponent(str));
+}
+
+function decode (str) {
+	return decodeURIComponent(atob(str));
+}
