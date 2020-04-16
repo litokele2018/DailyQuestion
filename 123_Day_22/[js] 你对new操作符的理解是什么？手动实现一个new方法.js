781e7c34f5c005bc21e681943821f52866731755
litokele <litokele2018@gmail.com>
2020-04-16 22:49:31 +0800

    Day22

---
+++ 123_Day_22/[js] 你对new操作符的理解是什么？手动实现一个new方法.js
@@ -0,0 +1,20 @@
+// [js] 你对new操作符的理解是什么？手动实现一个new方法
+class Person {
+  constructor(name, age) {
+    this.name = name
+    this.age = age
+  }
+}
+
+function myNew(xx, ...args) {
+  let result = xx(...args)
+  if (typeof result === 'object') {
+    return result
+  }
+  let obj = {}
+  obj.__proto__ = xx.prototype
+  xx.call(obj, ...args)
+  return obj
+}
+
+let x = myNew(Person, 'litokele', 18)
