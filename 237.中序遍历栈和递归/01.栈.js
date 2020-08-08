5f3ee3eacc3b89a9facd0d35ab24c91c2bb44a41
宿舍 <376762830@qq.com>
2020-08-08 20:12:42 +0800

    斗鱼笔试

--- 237.中序遍历栈和递归/01.栈.js
+++ 237.中序遍历栈和递归/01.栈.js
@@ -1,4 +1,14 @@
 // 中序遍历
-function inorder() {
-
+function inorder(root, op) {
+  let cur = root;
+  let stack = [];
+  while (cur || stack.length) {
+    while (cur) {
+      stack.push(cur);
+      cur = cur.left;
+    }
+    cur = stack.pop();
+    op.call(null, cur);
+    cur = cur.right;
+  }
 }
